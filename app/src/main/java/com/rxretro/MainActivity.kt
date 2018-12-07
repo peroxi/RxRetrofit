7689a649ad58dae4a199192ce347465c0bee1ca8
stanislav.semukhin <stanislav.semukhyn@numbrs.com>
2018-12-07 13:59:19 +0200

    Background and UI threads interactions implemented.

--- app/src/main/java/com/rxretro/MainActivity.kt
+++ app/src/main/java/com/rxretro/MainActivity.kt
@@ -1,14 +1,33 @@
 package com.rxretro
 
+import android.annotation.SuppressLint
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.rxretro.model.usecases.UseCaseFacade
 
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        UseCaseFacade.fetchContributorsOfUsersRepositories("eugenp", applicationContext)
+        val user = "eugenp"
+        UseCaseFacade.fetchContributorsOfUsersRepositories(user, applicationContext)
+            .doOnNext {
+                //Operations with the LIST of Contributors on UI thread
+                // (like setting/updating RecyclerView/number badge)
+                Toast.makeText(
+                    applicationContext,
+                    String.format("Contributors number of %s repositories is %s", user, it.size), Toast.LENGTH_LONG
+                ).show()
+            }
+            .flatMapIterable { it }
+            .subscribe { name: String ->
+                //Operations with single Contributor instances on UI thread.
+                Log.i("Cont First internal from db: ", name)
+            }
+
     }
 }
--- app/src/main/java/com/rxretro/model/usecases/UseCaseFacade.kt
+++ app/src/main/java/com/rxretro/model/usecases/UseCaseFacade.kt
@@ -5,24 +5,36 @@ import android.arch.persistence.room.Room
 import android.content.Context
 import android.util.Log
 import com.rxretro.model.dao.AppDatabase
+import io.reactivex.BackpressureStrategy
+import io.reactivex.Flowable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.subjects.PublishSubject
 
 object UseCaseFacade {
+
     @SuppressLint("CheckResult")
-    fun fetchContributorsOfUsersRepositories(user: String, applicationContext: Context) {
+    fun fetchContributorsOfUsersRepositories(user: String, applicationContext: Context): Flowable<List<String>> {
+        val dbSubject: PublishSubject<List<String>> = PublishSubject.create()
         EventDataFlattener.fetchContributors(user, applicationContext)
             .distinct { contributor -> contributor.name }
             .doOnError { Log.e("Cont First", "error : " + it) }
             .doOnComplete {
-                val db = Room.databaseBuilder(
-                    applicationContext,
-                    AppDatabase::class.java, "database-name"
-                ).build()
-                db.repositoryDao().selectContributorsOfUsersRepositories(user)
-                    .flatMapIterable { it }
-                    .subscribe { name: String -> Log.i("Cont First internal from db: ", name)
-                    }
+                fetchFromDB(applicationContext, user).toObservable()
+                    .subscribe { dbSubject.onNext(it) }
                 //Looks like it started being called after calling onComplete() to each item of source Observable.
             }
+            .observeOn(AndroidSchedulers.mainThread())
             .subscribe { contributor -> Log.i("Cont First", contributor.name) }
+        return dbSubject.toFlowable(BackpressureStrategy.BUFFER)
+    }
+
+    @SuppressLint("CheckResult")
+    private fun fetchFromDB(applicationContext: Context, user: String): Flowable<List<String>> {
+        val db = Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java, "database-name"
+        ).build()
+        return db.repositoryDao().selectContributorsOfUsersRepositories(user)
+            .observeOn(AndroidSchedulers.mainThread())
     }
 }
